dc05472af7974763c2f6f7af46304543d116159e
Andrés Court <andres@andrescourt.com>
2023-02-20 09:11:54 -0500

    Removed unused files

---
+++ app/bca/test/page.tsx
@@ -0,0 +1,10 @@
+export default function TestHome() {
+    return (
+        <div className="text-dark m-4 p-4 bg-light">
+            <h1 className="font-bold text-3xl uppercase">Testing</h1>
+            <p className="text-gray-800">Testing saving in nvim with Ctrl+c</p>
+            <p className="text-gray-800">Another saving test without pressing Esc</p>
+            <p className="text-gray-800">Final test</p>
+        </div>
+    )
+}
