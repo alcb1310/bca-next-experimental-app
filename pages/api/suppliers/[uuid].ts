05f00905e965d5be778a57f2387ca31e2e1d25b3
Andrés Court <andres@andrescourt.com>
2023-02-24 21:10:23 -0500

    Feature: Supplier - Backend:  Update the supplier information

--- pages/api/suppliers/[uuid].ts
+++ pages/api/suppliers/[uuid].ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import getOneSupplier from '@/pages/helpers/supplier/getOneSupplier'
 import { validateLoginInformation } from '@/pages/helpers/users'
+import prisma from '@/prisma/client'
 import { ErrorInterface, SupplierResponseType } from '@/types'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
@@ -21,7 +22,54 @@ export default async function handler(
 
   if (req.method === 'GET') {
     const supplier = await getOneSupplier(supplierUuid, user.companyUuid)
+
+    if (supplier === null)
+      return res.status(404).json({
+        detail: {
+          errorStatus: 404,
+          errorDescription: 'Supplier not found',
+        },
+      })
+
     return res.status(200).json({ detail: supplier })
+  }
+
+  if (req.method === 'PUT') {
+    const supplier = await getOneSupplier(supplierUuid, user.companyUuid)
+
+    if (supplier === null)
+      return res.status(404).json({
+        detail: {
+          errorStatus: 404,
+          errorDescription: 'Supplier not found',
+        },
+      })
+
+    const { supplier_id, name, contact_name, contact_email, contact_phone } =
+      req.body
+
+    const response = await prisma.supplier.update({
+      where: {
+        uuid: supplierUuid,
+      },
+      data: {
+        supplier_id: supplier_id ? supplier_id : supplier.supplier_id,
+        name: name ? name : supplier.name,
+        contact_name: contact_name ? contact_name : supplier.contact_name,
+        contact_email: contact_email ? contact_email : supplier.contact_email,
+        contact_phone: contact_phone ? contact_phone : supplier.contact_phone,
+      },
+      select: {
+        uuid: true,
+        supplier_id: true,
+        name: true,
+        contact_name: true,
+        contact_email: true,
+        contact_phone: true,
+      },
+    })
+
+    return res.status(200).json({ detail: response })
   }
 
   res.status(500).json({
